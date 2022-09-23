f40b1542babdfb1399af0fab09161391e0fd421d
sojeongsojeong-park <psj90972@naver.com>
2022-09-24 00:47:41 +0900

    feat: implement confirm modal

--- src/views/alert/alert.js
+++ src/views/alert/alert.js
@@ -35,3 +35,43 @@ alertModal.addEventListener("click", (e) => {
     alertModal.style.display = "none";
   }
 });
+
+export const confirmModal = function (message, callback) {
+  alertModal.style.display = "block";
+  const alertContainer = document.createElement("div");
+  alertContainer.classList.add("alertModalContainer");
+
+  const alertH2 = document.createElement("h2");
+  const alertP = document.createElement("p");
+  alertH2.textContent = message;
+  alertH2.classList.add("alertH2");
+  alertP.textContent = "⚠️";
+  alertP.classList.add("alertP");
+
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("alertButtonContainer");
+  const cancelButtonForConfirm = document.createElement("button");
+  cancelButtonForConfirm.textContent = "취소";
+  cancelButtonForConfirm.classList.add("cancelButtonForConfirm");
+  cancelButtonForConfirm.addEventListener("click", (e) => {
+    e.preventDefault();
+    alertModal.style.display = "none";
+  });
+
+  const confirmButtonForConfirm = document.createElement("button");
+  confirmButtonForConfirm.textContent = "확인";
+  confirmButtonForConfirm.classList.add("confirmButtonForConfirm");
+  confirmButtonForConfirm.addEventListener("click", (e) => {
+    e.preventDefault();
+    alertModal.style.display = "none";
+    callback();
+  });
+
+  buttonContainer.appendChild(cancelButtonForConfirm);
+  buttonContainer.appendChild(confirmButtonForConfirm);
+  alertContainer.appendChild(alertP);
+  alertContainer.appendChild(alertH2);
+  alertContainer.appendChild(buttonContainer);
+
+  alertModal.appendChild(alertContainer);
+};
